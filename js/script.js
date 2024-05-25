a7f35421c0448423dac14bb7554410b277030a87
German Lacruz <lacruz.german@gmail.com>
2024-05-25 18:51:32 -0300

    Se agrega flecha para el modo responsivo y mejoras 25-05-2025

--- js/script.js
+++ js/script.js
@@ -28,3 +28,22 @@ document.getElementById("formContact").addEventListener("submit", function(event
  // document.getElementById("asunto").value ="";
   document.getElementById("message").value ="";
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var scrollDown = document.querySelector('.scroll-down');
+  var footer = document.querySelector('footer');
+
+  window.addEventListener('scroll', function() {
+    var scrollHeight = document.documentElement.scrollHeight;
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var clientHeight = document.documentElement.clientHeight;
+    var footerOffset = footer.offsetTop;
+
+    // Verifica si el usuario ha llegado al final de la página o al pie de página
+    if (scrollHeight - scrollTop === clientHeight || scrollTop + clientHeight >= footerOffset) {
+      scrollDown.style.display = 'none'; // Oculta la flecha si ha llegado al final o al pie de página
+    } else {
+      scrollDown.style.display = 'block'; // Muestra la flecha si no ha llegado al final o al pie de página
+    }
+  });
+});
